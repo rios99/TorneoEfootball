d222d4d4578da1d70e5d5541e383dcab74c8bc83
marco <marco@DESKTOP-29F3NOP>
2023-01-24 00:25:44 -0500

    Agregar archivos de proyecto.

---
+++ TorneoEfootball/InicioTorneo.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TorneoEfootball
+{
+    partial class frmInicioTorneo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicioTorneo));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DragControl = new TorneoEfootball.DragControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TorneoEfootball.Properties.Resources.uefa_champions_league;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSalir, "btnSalir");
+            this.btnSalir.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnIniciar.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnIniciar, "btnIniciar");
+            this.btnIniciar.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelHeader.Controls.Add(this.label1);
+            resources.ApplyResources(this.panelHeader, "panelHeader");
+            this.panelHeader.Name = "panelHeader";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label1.Name = "label1";
+            // 
+            // DragControl
+            // 
+            this.DragControl.SelectControl = this.pictureBox1;
+            // 
+            // frmInicioTorneo
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.panelHeader);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.DarkViolet;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmInicioTorneo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnSalir;
+        private Button btnIniciar;
+        private Panel panelHeader;
+        private Label label1;
+        private DragControl DragControl;
+    }
+}
